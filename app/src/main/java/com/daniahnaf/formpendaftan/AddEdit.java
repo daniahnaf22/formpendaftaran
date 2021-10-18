e2f17e9ecefc0b3afe8539600f90b74454131bc1
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2021-10-18 22:10:07 +0700

    rubah jenis kelamin ke radio button

--- app/src/main/java/com/daniahnaf/formpendaftan/AddEdit.java
+++ app/src/main/java/com/daniahnaf/formpendaftan/AddEdit.java
@@ -1,24 +1,52 @@
 package com.daniahnaf.formpendaftan;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.daniahnaf.formpendaftan.helper.DbHelper;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
 
-public class AddEdit extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class AddEdit extends AppCompatActivity{
     DbHelper SQLite = new DbHelper(this);
-    String id, name, address, nohp, jk;
-    EditText txt_id, txt_name, txt_address, txt_nohp, txt_jk;
-    Button btnSubmit, btnCancel;
+    String id, name, address, nohp, jk, latitude, longitude;
+    EditText txt_id, txt_name, txt_address, txt_nohp;
+    TextView tvLokasi;
+    Button btnSubmit,btnGetLocation;
+    RadioButton rbLaki, rbPr;
+    RadioGroup rgGroup;
+    Boolean onJkSelected = false;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +58,18 @@ public class AddEdit extends AppCompatActivity {
         txt_name = findViewById(R.id.txt_name);
         txt_address = findViewById(R.id.txt_address);
         txt_nohp = findViewById(R.id.txt_nohp);
-        txt_jk = findViewById(R.id.txt_jk);
         btnSubmit = findViewById(R.id.btn_submit);
-        btnCancel = findViewById(R.id.btn_cancel);
+        btnGetLocation = findViewById(R.id.btnPilihLokasi);
+        rbLaki = findViewById(R.id.rbLaki);
+        rbPr = findViewById(R.id.rbPerempuan);
+        rgGroup = findViewById(R.id.rgGroup);
+        tvLokasi = findViewById(R.id.text_lokasi_sekarang);
 
         id = getIntent().getStringExtra(MainActivity.TAG_ID);
         name = getIntent().getStringExtra(MainActivity.TAG_NAME);
         address = getIntent().getStringExtra(MainActivity.TAG_ADDRESS);
         nohp = getIntent().getStringExtra(MainActivity.TAG_NOHP);
         jk = getIntent().getStringExtra(MainActivity.TAG_JK);
-
 
         if (id == null || id == "") {
             setTitle("Add Data");
@@ -49,8 +79,26 @@ public class AddEdit extends AppCompatActivity {
             txt_name.setText(name);
             txt_address.setText(address);
             txt_nohp.setText(nohp);
-            txt_jk.setText(jk);
+            if (jk.equals("Laki-laki")) {
+                rbLaki.setSelected(true);
+            } else {
+                rbPr.setSelected(true);
+            }
         }
+
+        btnGetLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    OnGPS();
+                } else {
+                    getLocation();
+                }
+            }
+        });
+
+        getRadioGroupValue();
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,19 +112,45 @@ public class AddEdit extends AppCompatActivity {
                 } catch (Exception ex) {
                     Log.e("Submit", ex.toString());
                 }
-
-                Intent intent = new Intent(AddEdit.this, MainActivity.class);
-                startActivity(intent);
             }
         });
 
-        btnCancel.setOnClickListener(new View.OnClickListener() {
+    }
+
+    void getLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (locationGPS != null) {
+                double lat = locationGPS.getLatitude();
+                double longi = locationGPS.getLongitude();
+                latitude = String.valueOf(lat);
+                longitude = String.valueOf(longi);
+                tvLokasi.setText("Your Location: " + "\n" + "Latitude: " + latitude + "\n" + "Longitude: " + longitude);
+            } else {
+                Toast.makeText(this, "Unable to find location.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void OnGPS() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("Yes", new  DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                blank();
-                finish();
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
             }
         });
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
     @Override
@@ -102,45 +176,67 @@ public class AddEdit extends AppCompatActivity {
         txt_id.setText("");
         txt_address.setText("");
         txt_nohp.setText("");
-        txt_jk.setText("");
         txt_id.requestFocus();
     }
 
 
     private void save() {
+        getRadioGroupValue();
         if (String.valueOf(txt_name.getText()).equals(null) || String.valueOf(txt_name.getText()).equals("") ||
                 String.valueOf(txt_address.getText()).equals(null) || String.valueOf(txt_address.getText()).equals("") ||
                 String.valueOf(txt_nohp.getText()).equals(null) || String.valueOf(txt_nohp.getText()).equals("") ||
-                String.valueOf(txt_jk.getText()).equals(null) || String.valueOf(txt_jk.getText()).equals("") ) {
+                !onJkSelected) {
             Toast.makeText(getApplicationContext(),
                     "Please input name or address ...", Toast.LENGTH_SHORT).show();
         } else {
             SQLite.insert(txt_name.getText().toString().trim(),
                     txt_address.getText().toString().trim(),
                     txt_nohp.getText().toString().trim(),
-                    txt_jk.getText().toString().trim());
+                    jk);
+            Intent intent = new Intent(AddEdit.this, MainActivity.class);
+            startActivity(intent);
             blank();
             finish();
         }
     }
 
     private void edit() {
+        getRadioGroupValue();
         if (String.valueOf(txt_name.getText()).equals(null) || String.valueOf(txt_name.getText()).equals("") ||
                 String.valueOf(txt_address.getText()).equals(null) || String.valueOf(txt_address.getText()).equals("") ||
                 String.valueOf(txt_nohp.getText()).equals(null) || String.valueOf(txt_nohp.getText()).equals("") ||
-                String.valueOf(txt_jk.getText()).equals(null) || String.valueOf(txt_jk.getText()).equals("")){
+                !onJkSelected) {
             Toast.makeText(getApplicationContext(),
-                    "Please input nam eor address ...",Toast.LENGTH_SHORT).show();
+                    "Please input name or address ...", Toast.LENGTH_SHORT).show();
         } else {
             SQLite.update(Integer.parseInt(txt_id.getText().toString()),
                     txt_name.getText().toString().trim(),
                     txt_address.getText().toString().trim(),
                     txt_nohp.getText().toString().trim(),
-                    txt_jk.getText().toString().trim());
+                    jk);
+            Intent intent = new Intent(AddEdit.this, MainActivity.class);
+            startActivity(intent);
             blank();
             finish();
         }
     }
 
-
+    private void getRadioGroupValue() {
+        rgGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.rbLaki:
+                        jk = rbLaki.getText().toString();
+//                        Toast.makeText(getApplicationContext(), "Laki-laki", Toast.LENGTH_SHORT).show();
+                        onJkSelected = true;
+                        break;
+                    case R.id.rbPerempuan:
+//                        Toast.makeText(getApplicationContext(), "Perempuan", Toast.LENGTH_SHORT).show();
+                        jk = rbPr.getText().toString();
+                        onJkSelected = true;
+                        break;
+                }
+            }
+        });
+    }
 }
